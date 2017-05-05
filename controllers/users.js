ac92434c50d82a056a410423465f6d97242eda6f
025609 <matthewnguyen@C1MRQ046HF4H.fios-router.home>
2017-05-04 23:59:43 -0400

    check credit card and fund children

--- controllers/users.js
+++ controllers/users.js
@@ -42,10 +42,50 @@ router.post('/updatedFund', function(req, res) {
       isSuccess: false
     });
   } else {
-    res.json({
-      error: "Credit Card number is valid.",
-      isSuccess: true
-    });
+
+
+    var children = req.body.children;
+
+    for (var i = 0; i < children.length; i++) {
+      // children[i]
+      if (children[i].amount) {
+        var amount = parseInt(children[i].amount);
+        var totalFund = children[i].funds += amount;
+        delete children[i].amount;
+        var childId = children[i]._id;
+        console.log(totalFund);
+
+        Children.findByIdAndUpdate(childId, {
+          funds: totalFund
+        }, {
+          new: true
+        }, function(err, model) {
+          if (err) {
+            error: err
+          };
+          console.log(model);
+          res.json({
+            error: "Credit Card number is valid.",
+            isSuccess: true
+          });
+
+        })
+
+
+
+
+
+      } // end if
+
+    } // end for loop
+
+
+
+
+    // res.json({
+    //   error: "Credit Card number is valid.",
+    //   isSuccess: true
+    // });
   }
   // let creditCardNumber = req.body.currentUser.creditCard;
   // let children = req.body.currentUser.child;
@@ -64,7 +104,27 @@ function checkCreditCard(cardNo) {
   if (cardNo.length !== 16) {
     return false;
   }
-  return true;
+  var validCard = function(num) {
+
+    var numberArr = num.toString().split('');
+
+    for (var i = numberArr.length - 2; i >= 0; i -= 2) {
+      numberArr[i] = numberArr[i] * 2;
+    }
+
+    numberArr = numberArr.join('').split('');
+
+    var summed = numberArr
+      .map(function(n) {
+        return n = parseInt(n);
+      })
+      .reduce(function(sum, m) {
+        return sum + m;
+      });
+
+    return summed % 10 == 0;
+  };
+  return validCard(cardNo);
 }
 
 module.exports = router;
